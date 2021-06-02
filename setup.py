a4469900e55ef4b24688baddd5c0e54864620c6f
hhoke <harris.hoke@10gen.com>
2021-06-02 14:43:41 -0400

    1604 -> 1804

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
             "arm/curator",
             "macos/curator",
             "macos-arm64/curator",
-            "ubuntu1604/curator",
+            "ubuntu1804/curator",
             "windows-64/curator.exe"
         ]
     }

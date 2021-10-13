f7b9b649202844e14689ce5c4b5c19ddb5544e22
hhoke <harris.hoke@10gen.com>
2021-10-13 17:21:16 -0400

    update with keywords

--- updateFromCurator.sh
+++ updateFromCurator.sh
@@ -3,9 +3,9 @@ set -xeou pipefail
 
 # updateFromCurator.sh allows automated hash updating.
 # It is intended to be used in the curatorbin root dir.
-# VERSION_UPDATE will increment the major/minor/patch versions as specified, with MAJOR.MINOR.PATCH
+# VERSION_UPDATE will increment the major/minor/patch versions as specified
 ## usage: ./updateFromCuratorCurator.sh ${VERSION_UPDATE}
-## usage: ./updateFromCuratorCurator.sh 0.1.0
+## usage: ./updateFromCuratorCurator.sh MAJOR
 
 NEW_HASH=$(git ls-remote git://github.com/mongodb/curator.git | head -n 1 | awk '{print $1}')
 old_hash=$(grep "VERSION_STRING =" testdir/test_basic.py | awk '{print $5}')
@@ -14,6 +14,20 @@ old_hash_stripped=${old_hash%\'}
 
 old_version=$(grep "version=" setup.py | awk -F '"' '{print $2}')
 
-replacement=$(echo ${old_version}.${1} | awk -F "." '{print $1+$4"."$2+$5"."$3+$6}')
 
+semVer=$(echo "${1}" | awk '{print tolower($0)}')
+
+if [ "{semVer}" == "major" ]
+then
+	replacement=$(echo ${old_version} | awk -F "." '{print $1+1".0.0"}')
+elif [ "{semVer}" == "minor" ]
+then
+	replacement=$(echo ${old_version} | awk -F "." '{print $1"."$2+1".0"}')
+elif [ "{semVer}" == "patch" ]
+then
+	replacement=$(echo ${old_version} | awk -F "." '{print $1"."$2"."$3+1}')
+
+else
+	echo "'${1}' is not major, minor, or patch"
+fi
 perl -p -i -e "s/${old_version}/${replacement}/g" setup.py
